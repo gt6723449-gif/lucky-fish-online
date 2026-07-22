f8a42b15b3b36405e8183d0976448dd2d27ceb1d
gt6723449 <gt6723449@gmail.com>
2026-07-22 22:50:39 +0300

    modifications

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const CONTACT_URL = 'https://www.time4100.com/affiliates/?btag=2627678';
 
-export const TARGET_SCORE = 5;
+export const TARGET_SCORE = 100;
 export const DEFAULT_PRIZE_AMOUNT = '100$';
 
 
